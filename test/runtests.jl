9823c1360ae6021f58bf992c957c17655ea2d5b7
Jun Tian <tianjun.cpp@gmail.com>
2019-01-01 22:12:10 +0800

    uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using Random:seed!
 
 @testset "Ju" begin
 
-# include("base.jl")
-# include("approximators.jl")
-# include("spaces.jl")
-# include("buffers.jl")
-# include("action_selectors.jl")
-# include("policies.jl")
-# include("environments.jl")
+include("base.jl")
+include("approximators.jl")
+include("spaces.jl")
+include("buffers.jl")
+include("action_selectors.jl")
+include("policies.jl")
+include("environments.jl")
 
 end
