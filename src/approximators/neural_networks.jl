c66c593b0f2848a0a10da115d72f72edc90e3866
Jun Tian <tianjun.cpp@gmail.com>
2019-01-14 01:43:50 +0800

    sync

--- src/approximators/neural_networks.jl
+++ src/approximators/neural_networks.jl
@@ -1,6 +1,28 @@
-struct NeuralNetworkQ{Tm}  <: AbstractQApproximator{Any, Int}
+using Flux
+
+struct NeuralNetworkQ{Tm, To, Tp}  <: AbstractQApproximator{Any, Int}
     model::Tm
+    opt::To
+    ps::Tp
+end
+
+function NeuralNetworkQ(model, opt)
+    ps = params(model)
+    NeuralNetworkQ(model, opt, ps)
 end
 
 (Q::NeuralNetworkQ)(s, ::Val{:dist}) = Q.model(s)
 (Q::NeuralNetworkQ)(s) = Q(s, Val(:dist))
+
+function (Q::NeuralNetworkQ)(s, ::Val{:max})
+    dist = Q(s)
+    maximum(dist, dims=ndims(dist))
+end
+
+function (Q::NeuralNetworkQ)(s, ::Val{:argmax})
+end
+
+function update!(Q::NeuralNetworkQ, loss)
+    back!(loss)
+    update!(Q.opt, Q.ps)
+end
