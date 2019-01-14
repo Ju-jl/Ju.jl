8f311927ff20812b09cee1fa51ee4f3bbb6fceac
Jun Tian <juti@microsoft.com>
2019-01-14 13:22:03 +0800

    sync

--- src/approximators/neural_networks.jl
+++ src/approximators/neural_networks.jl
@@ -4,23 +4,18 @@ struct NeuralNetworkQ{Tm, To, Tp}  <: AbstractQApproximator{Any, Int}
     model::Tm
     opt::To
     ps::Tp
-end
-
-function NeuralNetworkQ(model, opt)
-    ps = params(model)
-    NeuralNetworkQ(model, opt, ps)
+    function NeuralNetworkQ(model, opt)
+        m = gpu(model)
+        ps = params(m)
+        new(m, opt, ps)
+    end
 end
 
 (Q::NeuralNetworkQ)(s, ::Val{:dist}) = Q.model(s)
 (Q::NeuralNetworkQ)(s) = Q(s, Val(:dist))
-
-function (Q::NeuralNetworkQ)(s, ::Val{:max})
-    dist = Q(s)
-    maximum(dist, dims=ndims(dist))
-end
-
-function (Q::NeuralNetworkQ)(s, ::Val{:argmax})
-end
+(Q::NeuralNetworkQ)(s, ::Val{:argmax}) = map(i -> i[1], findmax(Q(s).data)[2])
+(Q::NeuralNetworkQ)(s, ::Val{:max}) = maximum(Q(s), dims=1)
+(Q::NeuralNetworkQ)(s, a) = Q(s)[a]
 
 function update!(Q::NeuralNetworkQ, loss)
     back!(loss)
---
+++ src/learners/dqn.jl
@@ -0,0 +1,30 @@
+using Flux
+
+const PolicyOrSelector = Union{AbstractPolicy, AbstractActionSelector}
+
+struct DQN{Tn<:NeuralNetworQ, Tp<:PolicyOrSelector} <: AbstractModelFreeLearner 
+    Q::Tn
+    π::Tp
+    γ::Float64
+    batch_size::Int
+end
+
+(learner::DQN{<:NeuralNetworQ, <:AbstractActionSelector})(s) = learner.Q(s) |> learner.π
+(learner::DQN{<:NeuralNetworQ, <:AbstractPolicy})(s) = learner.π(s)
+(learner::DQN{<:NeuralNetworQ, <:AbstractPolicy})(s, ::Val{:dist}) = learner.π(s, Val(:dist))
+
+function update!(learner::DQN{<:NeuralNetworQ, <:AbstractActionSelector}, buffer::CircularSARDBuffer)
+    Q, π, γ, batch_size = learner.Q, learner.π, learner.γ, learner.batch_size
+
+    if length(buffer) > batch_size
+        (s, a, r, d, s′), _ = sample(buffer, batch_size)
+        s, r, d, s′ = gpu(s), gpu(r), gpu(d), gpu(s′)  
+        a = map(i -> CartesianIndex(i, a[i]), eachindex(a))
+
+        q, q′ = Q(s, a), Q(s′, Val(:max))
+        G = @. r + γ * q′ * (1 - d)
+        loss = mse(G, q)
+        update!(Q, loss)
+        # update!(π, s, Q(s, Val(:argmax)))  # π isa AbstractPolicy
+    end
+end
