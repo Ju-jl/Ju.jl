0ec9e704c19063b9f8809de77f89bc1e1e17fe53
TianJun <tianjun@wisetechai.com>
2019-01-20 19:43:08 +0800

    add exp_decay for epsilon greedy

--- src/action_selectors/epsilon_greedy.jl
+++ src/action_selectors/epsilon_greedy.jl
@@ -4,6 +4,7 @@
     
 The best action is selected for a proportion ``1 - \\epsilon``
 and a random action (with uniform probability) is selected for a proportion ``\\epsilon``.
+ϵ can also be a decay. See the following examples.
 
 ## Example
 
@@ -18,11 +19,25 @@ Dict{Any,Int64} with 4 entries:
   3 => 22
   1 => 26
 ```
+
+julia> ϵ = exp_decay(init=1.0, λ=1.0, decay_step=500, clip=0.1)
+(::getfield(Ju, Symbol("#f#34")){Float64,Float64,Int64,Float64}) (generic function with 1 method)
+
+julia> selector = EpsilonGreedySelector(ϵ)
+EpsilonGreedySelector{getfield(Ju, Symbol("#f#34")){Float64,Float64,Int64,Float64}}(getfield(Ju, Symbol("#f#34")){Float64,Float64,Int64,Float64}(1.0, 1.0, 500, 0.1, Core.Box(-1)))
+
+julia> countmap(selector([1,2,1,1]) for _ in 1:1000)
+Dict{Any,Int64} with 4 entries:
+  4 => 101
+  2 => 677
+  3 => 106
+  1 => 116
 """
-mutable struct EpsilonGreedySelector <: AbstractActionSelector
-    ϵ::Float64
+mutable struct EpsilonGreedySelector{T<:Union{Float64, <:Function}} <: AbstractActionSelector
+    ϵ::T
 end
 
+_epsilon_select(values, ϵ) = rand() > ϵ ? sample(findallmax(values)[2]) : rand(1:length(values))
 
 """
     (p::EpsilonGreedySelector)(values::AbstractArray{T, 1}) where T
@@ -34,4 +49,6 @@ end
     `NaN` will be filtered unless all the values are `NaN`.
     In that case, a random one will be returned.
 """
-(p::EpsilonGreedySelector)(values::AbstractArray{<:Number, 1}) = rand() > p.ϵ ? sample(findallmax(values)[2]) : rand(1:length(values))
+(p::EpsilonGreedySelector{Float64})(values::AbstractArray{<:Number, 1}) = _epsilon_select(values, p.ϵ)
+
+(p::EpsilonGreedySelector{<:Function})(values::AbstractArray{<:Number, 1}) = _epsilon_select(values, p.ϵ())
