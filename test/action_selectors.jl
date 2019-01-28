281e9209233b7d2d450ea569b449fa878297be4f
Jun Tian <find_my_way@foxmail.com>
2019-01-28 23:59:16 +0800

    fix https://github.com/Ju-jl/ReinforcementLearningAnIntroduction.jl/i… (#27)
    * fix https://github.com/Ju-jl/ReinforcementLearningAnIntroduction.jl/issues/6
    
    * add test

--- test/action_selectors.jl
+++ test/action_selectors.jl
@@ -10,15 +10,30 @@
     end
     
     @testset "EpsilonGreedySelector" begin
-        seed!(123)
-        ϵ = 0.1
-        s = EpsilonGreedySelector(ϵ)
-        values = [1, 2, 0, -1]
-        action_counts = countmap(s(values) for _ in 1:1000)
-        @test isapprox(action_counts[1] / 1000, ϵ / length(values), atol=0.01)
-        @test isapprox(action_counts[2] / 1000, 1 - ϵ + ϵ / length(values), atol=0.01)
-        @test isapprox(action_counts[3] / 1000, ϵ / length(values), atol=0.01)
-        @test isapprox(action_counts[4] / 1000, ϵ / length(values), atol=0.01)
+        @testset "test distribution" begin
+            seed!(123)
+            ϵ = 0.1
+            s = EpsilonGreedySelector(ϵ)
+            values = [1, 2, 0, -1]
+            action_counts = countmap(s(values) for _ in 1:1000)
+            @test isapprox(action_counts[1] / 1000, ϵ / length(values), atol=0.01)
+            @test isapprox(action_counts[2] / 1000, 1 - ϵ + ϵ / length(values), atol=0.01)
+            @test isapprox(action_counts[3] / 1000, ϵ / length(values), atol=0.01)
+            @test isapprox(action_counts[4] / 1000, ϵ / length(values), atol=0.01)
+        end
+
+        # https://github.com/Ju-jl/ReinforcementLearningAnIntroduction.jl/issues/6
+        @testset "test generator as input" begin
+            seed!(123)
+            ϵ = 0.1
+            s = EpsilonGreedySelector(ϵ)
+            values = (i for i in (1,2,0,-1))
+            action_counts = countmap(s(values) for _ in 1:1000)
+            @test isapprox(action_counts[1] / 1000, ϵ / length(values), atol=0.01)
+            @test isapprox(action_counts[2] / 1000, 1 - ϵ + ϵ / length(values), atol=0.01)
+            @test isapprox(action_counts[3] / 1000, ϵ / length(values), atol=0.01)
+            @test isapprox(action_counts[4] / 1000, ϵ / length(values), atol=0.01)
+        end
     end
 
     @testset "UpperConfidneceBound" begin
